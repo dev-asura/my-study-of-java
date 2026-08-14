d33ba99a5ea32ceb98fc30d13d1c342752e21015
Asura <radfpsoficial@gmail.com>
2026-08-14 11:17:40 -0300

    This is my first java code.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I like ice-cream!");
+        System.out.print("It's really good!\n");
+        System.out.print("Buy me some ice-cream, please!");
+
+        // This's my first comment
+
+        /*
+        This
+        is
+        a
+        multi-line
+        comment
+         */
+    }
+}
