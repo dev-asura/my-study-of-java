da5b2370b3d6cbe6cad47f4a05b7b6d54c8f32a8
Asura <radfpsoficial@gmail.com>
2026-08-20 19:50:47 -0300

    Creating a program that prompts the user for their character's base attack, asking if they want a charge attack or a defensive attack. Based on this, the program will roll a twenty-sided dice and add the value to the character's attack.

--- src/module01_basics/exercises/aRPGDuel.java
+++ src/module01_basics/exercises/aRPGDuel.java
@@ -1,4 +1,54 @@
 package module01_basics.exercises;
-
+import java.util.Scanner;
+import java.util.Random;
 public class aRPGDuel {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int charAttack;
+        String posture;
+        int dice;
+        double damage;
+
+
+        System.out.print("Enter your character base attack: ");
+
+        while(!scanner.hasNextInt()) {
+            System.out.print("The value must be an integer number.\nEnter your character base attack: ");
+            scanner.next();
+        }
+
+        charAttack = scanner.nextInt();
+
+        scanner.nextLine();
+
+        do {
+            System.out.println(" X - Dashing Attack");
+            System.out.println(" Y - Defensive Attack");
+            System.out.print("Choose your posture: ");
+            posture = scanner.nextLine().trim();
+        } while (!posture.equalsIgnoreCase("X") && !posture.equalsIgnoreCase("Y"));
+
+        dice = random.nextInt(1, 21);
+        System.out.println("\nRolling the dice..." + dice);
+
+
+        if(dice == 20){
+            damage = charAttack * 2;
+            System.out.printf("\nAWESOME! Critical attack! Damage: %.2f", damage);
+        } else if(dice == 1){
+            damage = 0;
+            System.out.printf("\nBAD LUCK! You miss. Damage: %.2f", damage);
+        } else {
+            if(posture.equalsIgnoreCase("X")){
+                damage = charAttack + dice;
+                System.out.printf("\nYou got it! Damage: %.2f", damage);
+            } else {
+                damage = charAttack + Math.floor(dice / 2.0);
+                System.out.printf("\nYou git it! Damage: %.2f", damage);
+            }
+        }
+        scanner.close();
+    }
 }
