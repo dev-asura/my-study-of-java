aba262a1fb49bc2b8b2865849c66c14e88e3a95f
Asura <radfpsoficial@gmail.com>
2026-08-20 19:41:37 -0300

    Creating a Temperature Converter using Math methods and, Scanner and Random.

--- src/module01_basics/exercises/temperatureConverter.java
+++ src/module01_basics/exercises/temperatureConverter.java
@@ -1,4 +1,24 @@
 package module01_basics.exercises;
-
+import java.util.Scanner;
 public class temperatureConverter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Temperature Converter");
+        System.out.println("01 - Celsius to Fahrenheit");
+        System.out.println("02 - Fahrenheit to Celsius");
+
+
+
+
+
+
+
+
+
+
+
+        scanner.close();
+    }
 }
