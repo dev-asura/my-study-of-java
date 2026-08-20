2b3259d3522bdba13e1e4001f9c26204f9201d3d
Asura <radfpsoficial@gmail.com>
2026-08-20 19:45:25 -0300

    Creating a Badge and Promotion Coupon program. The sustem identifies the user's role and grants them a random coupon based on that role. It also generates a four-digit ID for the user's badge.

--- src/module01_basics/exercises/badgeAndPromotionalCouponGenerator.java
+++ src/module01_basics/exercises/badgeAndPromotionalCouponGenerator.java
@@ -1,4 +1,33 @@
 package module01_basics.exercises;
-
+import java.util.Scanner;
+import java.util.Random;
 public class badgeAndPromotionalCouponGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String name;
+        String role;
+        int badgeID;
+        int coupon;
+
+        System.out.println("Badge and Promotional Coupon Generator");
+        System.out.println("======================================");
+        System.out.print("Enter your full name: ");
+        name = scanner.nextLine().toUpperCase().trim();
+        name = name.substring(0, name.indexOf(" "));
+        System.out.print("Enter your role: ");
+        role = scanner.nextLine().toUpperCase().trim();
+        badgeID = random.nextInt(1000, 10000);
+
+        if(role.equalsIgnoreCase("DEVELOPER") || role.contains("DEV")){
+            role = "DEVELOPER";
+            coupon = random.nextInt(20, 51);
+            System.out.println("======================================");
+            System.out.printf("\nWelcome %s, your ID is: %d. Because you're a %s, you have a %d%% of discount coupon in our stores.", name, badgeID, role, coupon);
+        } else {
+            coupon = 10;
+            System.out.printf("\nWelcome %s, your ID is: %d. You have a %d%% discount coupon in our stores.", name, badgeID, coupon);
+        }
+        scanner.close();
+    }
 }
