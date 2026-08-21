97aa06b2e177f3ee7e644c945648601dec01c0a2
Asura <radfpsoficial@gmail.com>
2026-08-21 14:30:16 -0300

    Learning about 'while loop' and 'do while loop' and how to check conditions with it.

--- src/module01_basics/whileLoop.java
+++ src/module01_basics/whileLoop.java
@@ -1,4 +1,25 @@
 package module01_basics;
+import java.util.Scanner;
 
 public class whileLoop {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        // while loop = repeat some code forever
+        //              while some condition remains true.
+
+        String response = "";
+
+        System.out.println("Are u playing a game now!");
+        System.out.print("If u want to quit, press 'Q': ");
+        response = scanner.nextLine();
+
+        while(!response.equalsIgnoreCase("Q")){
+            System.out.print("Press 'Q' to quit the game: ");
+            response = scanner.nextLine();
+        }
+
+        System.out.println("\nYOU QUIT!");
+        scanner.close();
+    }
 }
