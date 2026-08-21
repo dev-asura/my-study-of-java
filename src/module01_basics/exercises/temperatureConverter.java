62be9078b586c93f3c1555b229f606f04cc4b85f
Asura <radfpsoficial@gmail.com>
2026-08-21 08:38:38 -0300

    Creating a Temperature Converter using do while loop, if statements, String methods (.equals), and Scanner.

--- src/module01_basics/exercises/temperatureConverter.java
+++ src/module01_basics/exercises/temperatureConverter.java
@@ -5,19 +5,30 @@ public class temperatureConverter {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Temperature Converter");
-        System.out.println("01 - Celsius to Fahrenheit");
-        System.out.println("02 - Fahrenheit to Celsius");
+        String option;
 
+        do {
+            System.out.println("Temperature Converter");
+            System.out.println("\n01 - Celsius to Fahrenheit");
+            System.out.println("02 - Fahrenheit to Celsius");
+            System.out.print("\nChoose an option (01 / 02): ");
+            option = scanner.nextLine().trim();
+        } while(!option.equals("01") && !option.equals("02") && !option.equals("1") && !option.equals("2"));
 
-
-
-
-
-
-
-
-
+        System.out.println(option);
+            if (option.equals("01") || option.equals("1")) {
+                System.out.print("Enter the temperature in Celsius: ");
+                double temperature = scanner.nextDouble();
+                double converted = temperature * (9.0 / 5.0) + 32.0;
+                System.out.printf("\n%.2f °C -> %.2f °F.", temperature, converted);
+            } else if (option.equals("02") || option.equals("2")) {
+                System.out.println("Enter the temperature in Fahrenheit: ");
+                double temperature = scanner.nextDouble();
+                double converted = (temperature - 32.0) * (5.0 / 9.0);
+                System.out.printf("\n%.2f °F -> %.2f °C.", temperature, converted);
+            } else {
+                System.out.println("Enter a valid option: ");
+            }
 
         scanner.close();
     }
