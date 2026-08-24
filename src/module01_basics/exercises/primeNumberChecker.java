a80522aa1aff4a91d0134cfa82e626e0fb17eec1
Asura <radfpsoficial@gmail.com>
2026-08-24 15:02:51 -0300

    I created a program that checks whether the user's input (a positive integer) is prime or not.

--- src/module01_basics/exercises/primeNumberChecker.java
+++ src/module01_basics/exercises/primeNumberChecker.java
@@ -1,4 +1,42 @@
 package module01_basics.exercises;
+import java.util.Scanner;
 
-public class cumulativeMultiplierAndCountdown {
+public class primeNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+        int count = 0;
+
+        while (true) {
+            System.out.print("Enter a positive integer number bigger than 0 (>= 1): ");
+
+            if(!scanner.hasNextInt()) {
+                System.out.println("Invalid input! That is not an integer.");
+                scanner.next();
+                continue;
+            }
+
+            number = scanner.nextInt();
+
+            if(number > 1){
+                break;
+            } else {
+                System.out.println("Invalid input! The number must be >= 1.");
+            }
+        }
+
+        for(int i = 1; i <= number; i++) {
+            if(number % i == 0){
+                count++;
+            }
+        }
+
+        if(count == 2) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " isn't a prime number.");
+        }
+        scanner.close();
+    }
 }
