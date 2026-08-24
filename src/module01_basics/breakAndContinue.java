f69021b0fa8324e92083dc21f3ae9819a1751f2d
Asura <radfpsoficial@gmail.com>
2026-08-24 14:46:29 -0300

    Learning about the Continue and Break commands and how they can skip or pause a loop.

--- src/module01_basics/breakAndContinue.java
+++ src/module01_basics/breakAndContinue.java
@@ -1,4 +1,10 @@
 package module01_basics;
 
 public class breakAndContinue {
+    public static void main(String[] args) {
+        // break = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
+
+
+    }
 }
