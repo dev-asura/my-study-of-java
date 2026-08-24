4fd0894dd6c69cf66815a6763e1cf7c7c125fef6
Asura <radfpsoficial@gmail.com>
2026-08-24 14:52:12 -0300

    I created a program that uses user input to calculate the factorial of a number.

--- src/module01_basics/exercises/factorialOfANumber.java
+++ src/module01_basics/exercises/factorialOfANumber.java
@@ -1,4 +1,22 @@
 package module01_basics.exercises;
+import java.util.Scanner;
 
 public class factorialOfANumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+        int factorial = 1;
+
+        System.out.print("This program will calculate the factorial of your positive integer number: ");
+        number = scanner.nextInt();
+
+        for(int i = number;i >= 1; i--){
+            factorial *= i;
+        }
+
+        System.out.println(number + "! = " + factorial);
+
+        scanner.close();
+    }
 }
