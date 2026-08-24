fb69c31444df5d2ff94b4d364ba09c64bbe763ad
Asura <radfpsoficial@gmail.com>
2026-08-24 14:58:26 -0300

    I created a program that use user input to calculate the multiplication table of a number.

--- src/module01_basics/exercises/multiplicationTablePersonality.java
+++ src/module01_basics/exercises/multiplicationTablePersonality.java
@@ -1,4 +1,24 @@
 package module01_basics.exercises;
+import java.util.Scanner;
 
 public class multiplicationTablePersonality {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int result;
+        System.out.print("Enter an integer number and this program will return the multiplication table from 1 to 10: ");
+
+        while(!scanner.hasNextInt()){
+            System.out.print("Enter invalid! Enter an integer number: ");
+            scanner.next();
+        }
+        number = scanner.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            result = i * number;
+            System.out.println(number + " * " + i + " = " + result);
+        }
+
+        scanner.close();
+    }
 }
