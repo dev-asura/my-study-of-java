3c4ea16e79f75f42327ccfe842caef60cec6184d
Asura <radfpsoficial@gmail.com>
2026-08-24 20:30:50 -0300

    Using what I learned about nested loops, I created a program that builds a centered pyramid using asterisks.

--- src/module01_basics/exercises/centralizedPyramidOfAsterisks.java
+++ src/module01_basics/exercises/centralizedPyramidOfAsterisks.java
@@ -5,7 +5,7 @@ public class centralizedPyramidOfAsterisks {
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
 
-        int height = random.nextInt(1, 11);
+        int height = random.nextInt(3, 11);
 
         System.out.println("This program will randomly select a number between 1 and 10. Then, it will construct a centralized pyramid of asterisks.");
         System.out.println("The random number is: " + height);
@@ -18,14 +18,15 @@ public class centralizedPyramidOfAsterisks {
         System.out.println(".");
         Thread.sleep(500);
 
-        for(int i = 1; i <= height; i++){
-            for(int j = 1; j <= height - i; j++){
-                System.out.print(" ");
-            }
-            for(int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-         }
+
+       for(int i = 1; i <= height; i++) {
+           for(int j = height - i; j >= 0; j--) {
+               System.out.print(" ");
+           }
+           for(int k = 1; k <= (i * 2 - 1) ; k++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+       }
     }
-}
