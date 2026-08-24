2faad49e8fb6793998945471d37d64d45cdcea4c
Asura <radfpsoficial@gmail.com>
2026-08-24 15:03:17 -0300

    I created a program that sum the even numbers between 0-100.

--- src/module01_basics/exercises/sumOfEvenNumbersFrom1To100.java
+++ src/module01_basics/exercises/sumOfEvenNumbersFrom1To100.java
@@ -1,4 +1,17 @@
 package module01_basics.exercises;
 
 public class sumOfEvenNumbersFrom1To100 {
+    public static void main(String[] args) {
+
+        int result = 0;
+
+        System.out.println("This program will calculate the sum of even number from 01 to 100.");
+
+        for(int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                result += i;
+            }
+        }
+        System.out.println(result);
+    }
 }
