090ed992a1eb26460282f9242a3ee8311126d1d5
Asura <radfpsoficial@gmail.com>
2026-09-01 14:14:52 -0300

    Learning and practicing the use of arrays to store user input.

---
+++ src/module01_basics/exercises/userInputIntoArrays.java
@@ -0,0 +1,27 @@
+package module01_basics.exercises;
+import java.util.Scanner;
+
+public class userInputIntoArrays {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String[] foods;
+        int size;
+
+        System.out.print("What number of food do you want: ");
+        size = scanner.nextInt();
+        scanner.nextLine();
+        foods = new String[size];
+
+        for(int i = 0; i < foods.length; i++){
+            System.out.print("Enter a food: ");
+            foods[i] = scanner.nextLine();
+        }
+
+        for(String food : foods) {
+            System.out.println(food);
+        }
+
+        scanner.close();
+    }
+}
