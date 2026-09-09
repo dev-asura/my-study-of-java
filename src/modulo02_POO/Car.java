bf79797aa2e4730601c6f787af1a5a962fa10810
Asura <radfpsoficial@gmail.com>
2026-09-09 09:39:50 -0300

    Learning more about objects, class, atributes and methods.

--- src/modulo02_POO/Car.java
+++ src/modulo02_POO/Car.java
@@ -1,3 +1,5 @@
+package modulo02_POO;
+
 public class Car {
 
     String make = "Porsche";
--- src/modulo02_POO/objects.java
+++ src/modulo02_POO/objects.java
@@ -1,5 +1,4 @@
-import java.util.Random;
-import java.util.Scanner;
+package modulo02_POO;
 
 public class objects {
     public static void main(String[] args){
